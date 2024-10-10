47a8260a5d6975aec7d5c0884b67f9b3842f6dad
Volhabo <barouskayavolha@gmail.com>
2024-10-10 23:07:11 +0200

    allure draft

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,43 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { tag, severity, description, link, step, tms, issue } from 'allure-js-commons';
 
 test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    await tag('Title');
+    await severity('critical');
+    await description('First test');
+    await link('/link1');
+    await link('/link2');
+
+    await step('Go to "https://playwright.dev/".', async () => {
+        await page.goto('https://playwright.dev/');
+    });
 
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    await step('Verify the title contains a substring "Playwright".', async () => {
+        await expect(page).toHaveTitle(/Playwright/);
+    });
 });
 
 test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    await tag('Installation');
+    await severity('minor');
+    await description('Second test');
+    await link('link1');
+    await issue('link2');
+    await tms('link3');
+
+    await step('Go to "https://playwright.dev/".', async () => {
+        await page.goto('https://playwright.dev/');
+    });
 
     // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
+    await step('Click the get started link', async () => {
+        await page.getByRole('link', { name: 'Get started' }).click();
+    });
 
     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await step('Verify the page has a heading with the name of Installation.', async () => {
+        await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
 });
