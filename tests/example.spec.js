66c59c2b66c04f5a3f4f7af8f62eb456e7b7ed25
Volhabo <barouskayavolha@gmail.com>
2024-10-13 22:42:51 +0200

    allure draft(add screen,video,issue,tms,epic)

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,17 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-import { tag, severity, description, link, step, tms, issue } from 'allure-js-commons';
+import { tag, displayName, epic, feature, severity, description, link, step, tms, issue, suite, subSuite } from 'allure-js-commons';
 
 test('has title', async ({ page }) => {
-    await tag('Title');
+    await displayName('Has title (displayName)');
+    await tag('Title (keywords)');
     await severity('critical');
-    await description('First test');
-    await link('/link1');
-    await link('/link2');
-
+    await description('Test description');
+    await issue('/link1', 'Name QASE link ');
+    await tms('/link2', 'Name ATC link');
+    await epic("Name epic1 (section)");
+    await feature("Name feature1");
+    
     await step('Go to "https://playwright.dev/".', async () => {
         await page.goto('https://playwright.dev/');
     });
@@ -20,12 +23,14 @@ test('has title', async ({ page }) => {
 });
 
 test('get started link', async ({ page }) => {
-    await tag('Installation');
+    await displayName('Get started link (displayName)');
+    await tag('Link');
     await severity('minor');
-    await description('Second test');
-    await link('link1');
-    await issue('link2');
-    await tms('link3');
+    await description('Test description');
+    await issue('/link1', 'Name QASE link ');
+    await tms('/link2', 'Name ATC link');
+    await epic("Name epic1 (section)");
+    await feature("Name feature2");
 
     await step('Go to "https://playwright.dev/".', async () => {
         await page.goto('https://playwright.dev/');
@@ -39,5 +44,48 @@ test('get started link', async ({ page }) => {
     // Expects page to have a heading with the name of Installation.
     await step('Verify the page has a heading with the name of Installation.', async () => {
         await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
+});
+
+test('has title fail', async ({ page }) => {
+    await tag('Title (keywords)');
+    await severity('normal');
+    await description('Test description');
+    await issue('/link1', 'Name QASE link ');
+    await tms('/link2', 'Name ATC link');
+    await epic("Name epic2 (section)");
+    await feature("Name feature1");
+    
+    await step('Go to "https://playwright.dev/".', async () => {
+        await page.goto('https://playwright.dev/');
+    });
+
+    // Expect a title "to contain" a substring.
+    await step('Verify the title contains a substring "Playwright".', async () => {
+        await expect(page).toHaveTitle(/APlaywright/);
+    });
+});
+
+test('get started link fail', async ({ page }) => {
+    await tag('Link (keywords)');
+    await severity('blocker');
+    await description('Test description');
+    await issue('/link1', 'Name QASE link ');
+    await tms('/link2', 'Name ATC link');
+    await epic("Name epic2 (section)");
+    await feature("Name feature2");
+
+    await step('Go to "https://playwright.dev/".', async () => {
+        await page.goto('https://playwright.dev/');
+    });
+
+    // Click the get started link.
+    await step('Click the get started link', async () => {
+        await page.getByRole('link', { name: 'Get started' }).click();
+    });
+
+    // Expects page to have a heading with the name of Installation.
+    await step('Verify the page has a heading with the name of Installation.', async () => {
+        await expect(page.getByRole('heading', { name: '_Installation' })).toBeVisible();
     });
 });
