548d0295c97876544195af8d9467738f6b4fcae2
Volhabo <99760694+Volhabo@users.noreply.github.com>
2024-10-20 23:36:00 +0200

    add allure (#4)
    * add allure

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,46 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { tags, epic, feature, severity, description, step, tms, issue } from 'allure-js-commons';
 
-test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+test('TC_00_01_Has title', async ({ page }) => {
+    await tags('Title (keywords)');
+    await severity('critical');
+    await description('Test description');
+    await issue('/link1', 'Name QASE link ');
+    await tms('/link2', 'Name ATC link');
+    await epic('Name epic1 (section)');
+    await feature('Name feature1');
+
+    await step('Go to "https://playwright.dev/".', async () => {
+        await page.goto('https://playwright.dev/');
+    });
 
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    await step('Verify the title contains a substring "Playwright".', async () => {
+        await expect(page).toHaveTitle(/Playwright/);
+    });
 });
 
-test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+test('TC_00_02_Get started link', async ({ page }) => {
+    await tags('Link', 'Keywords');
+    await severity('minor');
+    await description('Test description');
+    await issue('/link1', 'Name QASE link ');
+    await tms('/link2', 'Name ATC link');
+    await epic('Name epic1 (section)');
+    await feature('Name feature2');
+
+    await step('Go to "https://playwright.dev/".', async () => {
+        await page.goto('https://playwright.dev/');
+    });
 
     // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
+    await step('Click the get started link', async () => {
+        await page.getByRole('link', { name: 'Get started' }).click();
+    });
 
     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await step('Verify the page has a heading with the name of Installation.', async () => {
+        await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
 });
