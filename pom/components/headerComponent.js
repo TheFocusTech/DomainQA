a22d52d14989820e354692c0fe8b875e292a1f73
Elgrueber <elenagrueber@gmail.com>
2024-10-23 19:25:54 -0400

    fix format

--- pom/components/headerComponent.js
+++ pom/components/headerComponent.js
@@ -1,9 +1,5 @@
-import { step } from 'allure-js-commons';
-
 export default class HeaderComponent {
     constructor(page) {
         this.page = page;
-
-        this.trash = this.page.getByRole('link', { name: 'Trash' });
-    }  
+    }
 }
