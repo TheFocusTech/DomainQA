6081cd2d34e05819c4ea4dbb3437e344b5b5c4f6
Elgrueber <89706559+Elgrueber@users.noreply.github.com>
2024-10-23 10:26:35 -0400

    Registration/tc_01_02/incorrect_credentials_existing_email (#6)
    * add registration test with invalid data
    
    * change locators
    
    * fix format
    
    * add links and fix inaccuracies

--- testData.js
+++ testData.js
@@ -3,4 +3,5 @@ export const GOOGLE_DOC_LINK =
     'https://docs.google.com/document/d/1ff46TvvPYvH_AZfc4Ilqv6Dbo9Jxp31GQh-IJhrr0wc/edit?tab=t.0#heading=h.';
 export const URL_ENDPOINT = {
     login: '/auth/sign-in',
+    signup: '/auth/sign-up',
 };
