058703f8d5ea59144565b72fd2c338461398b088
Anna Afanaseva <annaafanasiewa@gmail.com>
2024-10-23 10:40:30 -0400

    TC_02_01

--- testData.js
+++ testData.js
@@ -4,3 +4,13 @@ export const GOOGLE_DOC_LINK =
 export const URL_ENDPOINT = {
     login: '/auth/sign-in',
 };
+
+export const INVALID_CREDS_AUTH = [
+    { email: 'invalidEmail@gmail.com', password: process.env.USER_PASSWORD, name: 'invalid email and valid password' },
+    { email: process.env.USER_EMAIL, password: 'invalidPassword', name: 'valid email and invalid password' },
+    { email: 'invalidEmail@gmail.com', password: 'invalidPassword', name: 'invalid email and invalid password' },
+];
+
+export const COLORS = {
+    red: 'rgb(242, 62, 62)',
+};
