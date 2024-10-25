aa8cb4d19877af4626fc8d3d474b249b1e65b2ca
Elgrueber <elenagrueber@gmail.com>
2024-10-25 19:15:42 -0400

    add test for registratoin with invalid email + rf allure in registration with existing email

--- tests/01Registration.spec.js
+++ tests/01Registration.spec.js
@@ -1,6 +1,7 @@
+import { expect } from '@playwright/test';
 import { test } from '../fixtures';
 import { description, tags, severity, epic, step, tms, issue } from 'allure-js-commons';
-import { QASE_LINK, GOOGLE_DOC_LINK, URL_ENDPOINT } from '../testData';
+import { QASE_LINK, GOOGLE_DOC_LINK, URL_ENDPOINT, NEGATIVE_EMAIL_DATA_SET } from '../testData';
 
 test.describe('Registration', () => {
     test('TC_01_02_01 | Verify user is not able to sign up with an existing email', async ({
@@ -22,11 +23,45 @@ test.describe('Registration', () => {
         await step('Verify user is on Sign up page.', async () => {
             await page.waitForURL(process.env.URL + URL_ENDPOINT.signup);
         });
-        await signupPage.fillEmailAddressInput(process.env.USER_EMAIL);
-        await signupPage.selectCheckboxReceiveEmails();
-        await signupPage.clickCreateAccount();
+        await step('Verify the filling existing Email Address.', async () => {
+            await signupPage.fillEmailAddressInput(process.env.USER_EMAIL);
+        });
+        await step('Verify the select of check box to receive emails.', async () => {
+            await signupPage.selectCheckboxReceiveEmails();
+        });
+        await step('Verify the click the "Create account" button.', async () => {
+            await signupPage.clickCreateAccount();
+        });
         await step('Verify the registration is failed.', async () => {
-            await signupPage.errorMessageEmailExist.isVisible();
+            await signupPage.errorMessageEmail.isVisible();
+        });
+    });
+    NEGATIVE_EMAIL_DATA_SET.forEach((typeEmailField) => {
+        test(`TC_01_02_02 | Verify user is not able to sign up with an invalid email: ${typeEmailField[0]}`, async ({
+            page,
+            homePage,
+            signupPage,
+        }) => {
+            await tags('Registration', 'Negative');
+            await severity('normal');
+            await description('Verify that registration fails when an invalid email is used');
+            await issue(`${QASE_LINK}suite=1&case=4`, 'User Registration');
+            await tms(`${GOOGLE_DOC_LINK}kjwdmepb5t7q`, 'ATC_01_02_02');
+            await epic('Registration');
+
+            await step('Navigate to Home page.', async () => {
+                await page.goto('/');
+            });
+            await homePage.clickSignup();
+            await step('Verify user is on Sign up page.', async () => {
+                await page.waitForURL(process.env.URL + URL_ENDPOINT.signup);
+            });
+            await step('Verify the filling invalid Email Address.', async () => {
+                await signupPage.fillEmailAddressInput(typeEmailField[1]);
+            });
+            await step('Verify the error message.', async () => {
+                await expect(signupPage.errorMessageEmail).toHaveText(typeEmailField[2]);
+            });
         });
     });
 });
