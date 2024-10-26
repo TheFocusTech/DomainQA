134221cc2d9ad0196a1f5381c3bb41df7dad0b29
Anna Afanaseva <annaafanasiewa@gmail.com>
2024-10-26 15:05:09 -0400

    add check for count

--- helpers/apiCalls.js
+++ helpers/apiCalls.js
@@ -1,6 +1,26 @@
 import { API_ENDPOINT } from '../testData';
 import { getRandomDomainName } from './utils';
 
+export async function getHostedZonesAPI(request, headers) {
+    const { 'dr.token': authToken, 'dr.scrf-token': xsrfToken, _csrf: csrfCookie } = headers;
+    const getHostedZonesResponse = await request.get(`${process.env.API_URL}/users/hosted-zones?`, {
+        // TODO:
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+            'x-csrf-token': xsrfToken,
+            Cookie: `_csrf=${csrfCookie}`,
+        },
+    });
+    if (!getHostedZonesResponse.ok()) {
+        console.error(`Failed to get hosted zones: ${getHostedZonesResponse.statusText()}`);
+    }
+    const hostedZonesData = await getHostedZonesResponse.json();
+    const hostedZoneCount = hostedZonesData.meta.total;
+    console.log(`Total number of hosted zones: ${hostedZoneCount}`);
+
+    return hostedZoneCount;
+}
+
 export async function createHostedZoneAPI(request, headers) {
     const domainName = await getRandomDomainName();
     const { 'dr.token': authToken, 'dr.scrf-token': xsrfToken, _csrf: csrfCookie } = headers;
--- tests/04Domains.spec.js
+++ tests/04Domains.spec.js
@@ -1,12 +1,14 @@
 import { test } from '../fixtures';
 import { URL_ENDPOINT } from '../testData';
 import { loginUser } from '../helpers/preconditions';
-import { createHostedZoneAPI, deleteHostedZoneAPI } from '../helpers/apiCalls';
+import { createHostedZoneAPI, deleteHostedZoneAPI, getHostedZonesAPI } from '../helpers/apiCalls';
 import { getCookies } from '../helpers/utils';
 import { step } from 'allure-js-commons';
+import { expect } from 'allure-playwright';
 let headers;
 let newHostedZoneId;
 let domainName;
+let hostedZoneCount;
 
 test.describe('Domains', () => {
     test.skip('TC_04_02 | Verify search by hosted zone name.', async ({
@@ -23,9 +25,12 @@ test.describe('Domains', () => {
 
         await step('Preconditions: Create hosted zone via API.', async () => {
             headers = await getCookies(page);
+
             const createdHostedZoneResponse = await createHostedZoneAPI(request, headers);
             newHostedZoneId = createdHostedZoneResponse.id;
             domainName = createdHostedZoneResponse.domain;
+
+            hostedZoneCount = await getHostedZonesAPI(request, headers);
 
             await page.goto(URL_ENDPOINT.hostedZones);
         });
@@ -34,6 +39,10 @@ test.describe('Domains', () => {
 
         await step('Postconditions: Delete hosted zone via API.', async () => {
             await deleteHostedZoneAPI(request, newHostedZoneId, headers);
+
+            const hostedZoneCountAfter = await getHostedZonesAPI(request, headers);
+
+            expect(hostedZoneCountAfter).toEqual(hostedZoneCount - 1);
         });
     });
 });
