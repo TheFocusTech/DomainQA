7c8beb4fbde70099ddc81e1f46dbf832249ae08d
Elgrueber <elenagrueber@gmail.com>
2024-10-28 10:15:25 -0400

    remove not used code

--- pom/pages/HomePage.js
+++ pom/pages/HomePage.js
@@ -4,22 +4,8 @@ export default class HomePage {
     constructor(page) {
         this.page = page;
 
-        this.loginButton = this.page.getByRole('link', { name: 'Log in' });
-        this.signupButton = this.page.getByRole('link', { name: 'Join now' });
         this.domainSearchInput = this.page.getByPlaceholder('Search domain').first();
         this.searchButton = this.page.getByRole('button', { name: 'Search' }).nth(1);
-    }
-
-    async clickLogin() {
-        await step('Click on "Log in" button.', async () => {
-            await this.loginButton.click();
-        });
-    }
-
-    async clickSignup() {
-        await step('Click on "Join now" button.', async () => {
-            await this.signupButton.click();
-        });
     }
 
     async fillDomainSearchInput(nameDomain) {
