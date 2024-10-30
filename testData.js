fe7ecc3bdc8a5f7372670d182c8ce788e9e703ce
Elgrueber <89706559+Elgrueber@users.noreply.github.com>
2024-10-30 11:03:23 -0400

    Unauthorized user/09 02 01/search available domain (#26)
    * add test for search
    
    * remove comments

--- testData.js
+++ testData.js
@@ -63,6 +63,10 @@ export const ERROR_DOMAIN = 'FGGFFGDGDFGD.COM';
 
 export const CORRECT_DOMAIN = 'PURRWEB.COM';
 
+export const NON_AVAILABLE_DOMAIN = 'PURRWEB.net';
+
+export const AVAILABLE_DOMAIN = 'strain123';
+
 export const ACCESSIBLE_PAGE_TITLE = {
     Transfer: 'Transfer your .com domain to Trustname for $12.99',
     HomePage: 'Millions of domains with the ulitmate privacy protection',
@@ -123,3 +127,5 @@ export const DNS_RECORD_DATA = {
     },
     comment: 'For Test Only!',
 };
+
+export const DOMAIN_PART = ['.com', '.net', 'org'];
