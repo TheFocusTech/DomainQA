18b80a45bba8657c44fd6fb5e6b58dba64a7fbfd
Anna Afanaseva <annaafanasiewa@gmail.com>
2024-11-13 11:55:00 -0500

    revert config changes, comment failing check

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 export default defineConfig({
     testDir: './tests',
     /* Run tests in files in parallel */
-    fullyParallel: false,
+    fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
