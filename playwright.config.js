9e080550a59bdb09c247ae33cdd3f8e8a71134e9
ViktoriyaED <qaviktoriaed@gmail.com>
2024-11-14 11:59:03 -0500

    Add GitHub Actions Reporter #2

--- playwright.config.js
+++ playwright.config.js
@@ -25,6 +25,8 @@ export default defineConfig({
     reporter: [
         ['html'],
         ['line'],
+        ['list'],
+        ['@estruyf/github-actions-reporter'],
         [
             'allure-playwright',
             {
