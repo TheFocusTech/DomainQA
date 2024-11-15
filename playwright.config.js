88f019b54c545875cc3df5dcaf3f4961a00335cf
ViktoriyaED <qaviktoriaed@gmail.com>
2024-11-14 21:53:14 -0500

    GitHub Actions Reporter for Playwright

--- playwright.config.js
+++ playwright.config.js
@@ -23,17 +23,8 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-        // ['html'],
-        // ['line'],
-        ['list'],
-        [
-            '@estruyf/github-actions-reporter',
-            {
-                title: 'Playwright test results',
-                useDetails: true,
-                showError: true,
-            },
-        ],
+        ['html'],
+        ['line'],
         [
             'allure-playwright',
             {
