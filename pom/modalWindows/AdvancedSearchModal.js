662a6174d4b3a933c92df7136e71fc98b25f49c4
SvetlanaMag <svetlanammmsv@gmail.com>
2024-11-16 21:29:55 +0300

    fix test

--- pom/modalWindows/AdvancedSearchModal.js
+++ pom/modalWindows/AdvancedSearchModal.js
@@ -7,21 +7,23 @@ export default class AdvancedSearchModal {
         this.page = page;
 
         this.categoryTLD = (category) => this.page.locator(`//button[span[text()="${category}"]]`);
+        this.firstTLD = this.page.locator(
+            'section[class*="tld-category-list"]>div:first-child span[class*="choicebox__text"]'
+        );
         this.TLDsList = this.page.locator('section[class*="tld-category-list"] span[class*="choicebox__text"]');
+        this.randomTLD = (index) =>
+            this.page.locator(
+                `section[class*="tld-category-list"]>div:nth-child(${index + 1}) span[class*="choicebox__text"]`
+            );
         this.applyButton = this.page.locator('button').filter({ hasText: 'Apply' });
-        this.modalWindow = this.page.locator('section[class*="modal-root"]');
+        this.advancedSearchHeader = this.page.locator('section[class*="modal-root"] h2');
         this.filterHeader = this.page.locator('span[class*="tld-header_filter-header"]:last-child');
     }
 
     async selectCategory(category) {
         await step(`Select Category ${category}`, async () => {
-            await this.categoryTLD(category).click();
-            await expect(this.categoryTLD(category)).toHaveCSS('color', 'rgb(0, 97, 75)');
-            const firstTLD = this.page.locator(
-                'section[class*="tld-category-list"]>div:first-child span[class*="choicebox__text"]'
-            );
-            await firstTLD.waitFor('visible');
-            await expect(firstTLD).toContainText(`.${category}`);
+            await this.categoryTLD(category).click({ forse: true });
+            await this.firstTLD.waitFor('visible');
         });
     }
 
@@ -31,11 +33,8 @@ export default class AdvancedSearchModal {
             const count = await this.TLDsList.count();
             let listOfTLDIndex = await getRandomArray(count, numberOfTLDs);
             for (const index of listOfTLDIndex) {
-                const randomTLD = await this.page.locator(
-                    `section[class*="tld-category-list"]>div:nth-child(${index + 1}) span[class*="choicebox__text"]`
-                );
+                const randomTLD = await this.randomTLD(index);
                 await randomTLD.click();
-                await expect(this.categoryTLD(category)).toHaveClass(/tld-item_tld-item--selected/);
                 listOfTLD.push(await randomTLD.textContent());
             }
         });
@@ -55,7 +54,9 @@ export default class AdvancedSearchModal {
 
             for (const category of categoriesArr) {
                 await this.selectCategory(category);
+                await expect(this.firstTLD).toContainText(`.${category}`);
                 let listOfTLDOneCategory = await this.selectTLDs(category, numberOfTLDs);
+                await expect(this.categoryTLD(category)).toHaveClass(/tld-item_tld-item--selected/);
                 listOfTLDs.push(...listOfTLDOneCategory);
             }
         });
