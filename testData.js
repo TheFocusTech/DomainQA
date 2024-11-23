84e3ca07522179dfe419c6ead66110f07baef77e
Iryna Kolhanova <iryna.kolhanova@gmail.com>
2024-11-22 19:32:20 -0500

    fix

--- testData.js
+++ testData.js
@@ -34,6 +34,11 @@ export const COLORS = {
     red: 'rgb(242, 62, 62)',
 };
 
+export const PASSWORD = {
+    password: `${process.env.USER_PASSWORD}`,
+    newPassword: `NEW_${process.env.USER_PASSWORD}`,
+};
+
 export const TOAST_MESSAGE = {
     passwordChanged: 'Password changed successfully',
     hostedZoneCreated: 'Hosted zone created',
